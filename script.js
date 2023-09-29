777e3835120d091e56656a6b31beda148751c666
CosmicTacoCat <tacocatdx@gmail.com>
2023-09-29 03:29:45 -0500

    added more timer logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let timer = document.querySelector(".timer");
 
-let seconds = 0;
+let seconds = 20
+;
 let minutes = 0;
 let interval = 1000;
 let expectedTime = Date.now() + interval;
@@ -9,7 +10,15 @@ function updateTimer() {
     if (drift > interval) {
         // Took too long. You can do whatever you need to make up for it here.
     } else { // Normal on-time behaviour.
-        
+        seconds -= 1;
+        if (seconds < 0) {
+            if (minutes > 0) {
+                minutes -= 1;
+                seconds = 59;
+            } else { // Time is up.
+                seconds = 0;
+            }
+        }
         timer.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2,"0")}`;
     }
 
