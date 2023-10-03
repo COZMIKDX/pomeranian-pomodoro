7c9b1c374105d0657d98ced21accea1cf453d852
CosmicTacoCat <tacocatdx@gmail.com>
2023-10-02 23:59:25 -0500

    bringing in code from a timer I made in codepen while at work.

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
-let timer = document.querySelector(".timer");
+class DynamicTimer {
+    constructor(interval, element) {
+        this.interval = interval;
+        this.element = element;
+        this.timerId = null;
+        this.minutes = 0;
+        this.seconds = 20;
+        this.expectedTime = 0;
 
-let seconds = 2;
-let minutes = 1;
-let interval = 1000;
-let expectedTime = Date.now() + interval;
-let timerId;
-function updateTimer() {
-    var drift = Date.now() - expectedTime; // how off the timer is.
-    if (drift > interval) {
-        // Took too long. You can do whatever you need to make up for it here.
-    } else { // Normal on-time behaviour.
-        seconds -= 1;
-        if (seconds < 0) {
-            if (minutes > 0) {
-                minutes -= 1;
-                seconds = 59;
-            } else { // Time is up.
-                seconds = 0;
-            }
-        }
-        timer.innerHTML = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2,"0")}`;
+        this.timerTick = this.timerTick.bind(this);
     }
 
-    expectedTime += interval; // The next time it should trigger.
-    timerId = setTimeout(updateTimer, Math.max(0, interval - drift)); // Either trigger immediately or adjust the interval based on drift.
+    updateUI() {
+        this.element.innerHTML = this.element.innerHTML = `${this.minutes.toString().padStart(2, "0")}:${this.seconds.toString().padStart(2, "0")}`;
+    }
+
+    timerTick() {
+        var drift = Date.now() - this.expectedTime;
+        if (drift > this.interval) {
+            // Took too long. Do anything needed to make up for being late.
+            console.log("Drift detected");
+        } else { // Normal on-time behaviour
+            this.seconds -= 1;
+            // Carry over a minute if we're out of seconds.
+            if (this.seconds < 0) {
+                if (this.minutes > 0) {
+                    this.minutes -= 1;
+                    this.seconds = 59;
+                } else {
+                    // Timer done
+                    this.seconds = 0;
+                    this.stopTimer();
+                }
+            }
+
+            this.updateUI();
+        }
+
+        this.expectedTime += this.interval; // The next time it should trigger.
+        // setTimout again but adjust the interval to make up for drift.
+        // If the drift was too big, set the next interval to 0.
+        this.timerId = setTimeout(this.timerTick, Math.max(0, this.interval - drift));
+    }
+
+    startTimer() {
+        this.expectedTime = Date.now() + this.interval;
+        this.timerId = setTimeout(this.timerTick, this.interval);
+    }
+
+    stopTimer() {
+        clearTimeout(this.timerId);
+    }
+
+    setTime(minutes, seconds) {
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.updateUI();
+    }
 }
 
-timerId = setTimeout(updateTimer, interval);
+let playButton = document.getElementById("play");
+let resetButton = document.getElementById("set");
+let timer = new DynamicTimer(1000, document.getElementById("timer1"));
+let playing = false;
+let cleanTimer = true;
 
-let buttons = document.querySelectorAll(".button");
-let leftButton = buttons[0];
-let rightButton = buttons[1];
-let play = false;
-
-leftButton.addEventListener("click", () => {
-
+playButton.addEventListener("click", () => {
+    if (!playing) {
+        timer.startTimer();
+        playing = true;
+        playButton.innerHTML = "PAUSE";
+    } else {
+        timer.stopTimer();
+        playing = false;
+        playButton.innerHTML = "START";
+    }
 });
 
-function startTimer(millisec_interval) {
-    timerId = setTimeout(updateTimer, millisec_interval);
-}
-
-function timerUserControlFSM() {
-    if (play == false) {
-        startTimer(interval);
-        play = true;
-    } else if (play == true) {
-        clearTimeout(timerId);
-
-    }
-}
+resetButton.addEventListener("click", () => {
+    timer.stopTimer();
+    timer.setTime(1, 20);
+});
